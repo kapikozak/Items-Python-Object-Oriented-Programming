d00551fffe06962193627b15dd3089abc023a500
kapikozak <kapikozak@gmail.com>
2026-05-07 21:17:01 +0200

    Typo fix

--- keyboard.py
+++ keyboard.py
@@ -1,7 +1,7 @@
 from item import Item
 
 class Keyboard(Item):
-    pay_rate = 0.6 # $40% discount for all keyboards
+    pay_rate = 0.6 # 40% discount for all keyboards
 
     def __init__(self, name: str, price: float, quantity=0):
         # Call to super function to have access to all class attributes and methods
